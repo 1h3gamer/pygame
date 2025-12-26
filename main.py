d9a93a985582aa389c796d0bbbc078982050ca30
1h3gamer <Idaune.nathan@gmail.com>
2025-12-26 13:32:04 +0000

    hw rect

--- main.py
+++ main.py
@@ -8,5 +8,5 @@ while not done:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             quit
-    pygame.draw.rect(screen,(0,125,225),pygame.Rect(30,30,200,300))
+    pygame.draw.rect(screen,(0,225,225),pygame.Rect(30,30,200,300))
     pygame.display.flip()
