0ea646bae5032e3d8ce9b2dadfa1e2418db244de
1h3gamer <Idaune.nathan@gmail.com>
2025-12-26 13:42:35 +0000

    hw rect 1

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import pygame
 
 pygame.init()
-screen = pygame.display.set_mode((400,400))
+screen = pygame.display.set_mode((640,480))
+pygame.display.set_caption("MY FIRST GAME SCREEN")
 
 done = False
 while not done:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             quit
-    pygame.draw.rect(screen,(0,225,225),pygame.Rect(30,30,200,300))
+    pygame.draw.rect(screen,(0,225,225),pygame.Rect(30,30,200,200))
     pygame.display.flip()
